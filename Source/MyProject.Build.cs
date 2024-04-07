a4d2128c4916d99aae81d9ab8f16de225d22152c
Colibri Shin <colibrishin@gmail.com>
2024-04-08 01:36:22 +0900

    feat: add support for cpp17

---
+++ Source/MyProject.Build.cs
@@ -0,0 +1,21 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class MyProject: ModuleRules
+{
+	public MyProject(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		CppStandard = CppStandardVersion.Cpp17;
+
+		PublicDependencyModuleNames.AddRange(new string[] 
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore"
+		});
+	}
+}
