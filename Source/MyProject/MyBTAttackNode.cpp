294dbaf2902a43e979b98378057df5248182856b
Colibri Shin <colibrishin@gmail.com>
2024-04-08 18:48:16 +0900

    feat: bt attack node

---
+++ Source/MyProject/MyBTAttackNode.cpp
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyProject/MyBTAttackNode.h"
+
+#include "AIController.h"
+#include "MyCharacter.h"
+
+#include "BehaviorTree/BlackboardComponent.h"
+
+UMyBTAttackNode::UMyBTAttackNode()
+	: bIsAttacking(false)
+{
+	bNotifyTick = true;
+
+}
+
+EBTNodeResult::Type UMyBTAttackNode::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	const auto& SuperResult = Super::ExecuteTask(OwnerComp , NodeMemory);
+	const auto& AIOwner = Cast<AMyCharacter>(OwnerComp.GetAIOwner()->GetPawn());
+	constexpr wchar_t TargetKey[] = L"Target";
+
+	if (!IsValid(AIOwner))
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	const auto& Target = Cast<AMyCharacter>(OwnerComp.GetBlackboardComponent()->GetValueAsObject(TargetKey));
+
+	if (!IsValid(Target))
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	AIOwner->BindOnAttackEnded(this , [this]()
+	{
+		bIsAttacking = false;
+	});
+
+	AIOwner->Attack();
+	bIsAttacking = true;
+
+	return SuperResult;
+}
+
+void UMyBTAttackNode::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp , NodeMemory , DeltaSeconds);
+
+	if (!bIsAttacking)
+	{
+		FinishLatentTask(OwnerComp , EBTNodeResult::Succeeded);
+	}
+}
