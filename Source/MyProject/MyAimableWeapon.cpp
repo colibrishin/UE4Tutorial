cf25c439ea97ff15867e9bbab404d8df4c280acc
Colibri Shin <colibrishin@gmail.com>
2024-04-09 19:18:16 +0900

    feat: add hitscan weapon

--- Source/MyProject/MyAimableWeapon.cpp
+++ Source/MyProject/MyAimableWeapon.cpp
@@ -3,3 +3,87 @@
 
 #include "MyProject/MyAimableWeapon.h"
 
+#include "DrawDebugHelpers.h"
+#include "MyCharacter.h"
+
+#include "Camera/CameraComponent.h"
+
+AMyAimableWeapon::AMyAimableWeapon()
+	: bCanFire(true)
+{
+}
+
+void AMyAimableWeapon::Fire()
+{
+	const auto& Character = GetItemOwner();
+
+	if (!IsValid(Character))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Character is not valid"));
+		return;
+	}
+
+	if (!bCanFire)
+	{
+		return;
+	}
+
+	const auto& Camera = Cast<UCameraComponent>(Character->GetComponentByClass(UCameraComponent::StaticClass()));
+
+	if (!IsValid(Camera))
+	{
+		return;
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("Fire!"));
+
+	FHitResult HitResult;
+	FCollisionQueryParams Params{NAME_None, false, Character};
+
+	FVector EndVector = Camera->GetComponentLocation() + Camera->GetForwardVector() * Range;
+
+	const auto& Result = GetWorld()->LineTraceSingleByChannel
+	(
+		OUT HitResult,
+		Camera->GetComponentLocation(),
+		EndVector,
+		ECollisionChannel::ECC_Visibility,
+		Params
+	);
+
+	if (Result)
+	{
+		DrawDebugLine(
+			GetWorld(),
+			Camera->GetComponentLocation(),
+			HitResult.ImpactPoint,
+			FColor::Green,
+			false,
+			1.f,
+			0,
+			2.f
+		);
+	}
+	else
+	{
+		DrawDebugLine(
+			GetWorld(),
+			Camera->GetComponentLocation(),
+			EndVector,
+			FColor::Green,
+			false,
+			1.f,
+			0,
+			2.f
+		);
+	}
+
+	GetWorld()->GetTimerManager().SetTimer(OUT FireRateTimerHandle, IN this, &AMyAimableWeapon::ResetFire, FireRate);
+}
+
+void AMyAimableWeapon::ResetFire()
+{
+	bCanFire = true;
+	GetWorld()->GetTimerManager().ClearTimer(FireRateTimerHandle);
+	OnFireReady.Broadcast();
+}
