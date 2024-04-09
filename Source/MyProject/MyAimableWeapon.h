eb62c5c2be63e8cc548def6c96fc9e76b7966d92
Colibri Shin <colibrishin@gmail.com>
2024-04-09 15:54:53 +0900

    feat: aimable weapon

---
+++ Source/MyProject/MyAimableWeapon.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MyProject/MyWeapon.h"
+#include "MyAimableWeapon.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API AMyAimableWeapon : public AMyWeapon
+{
+	GENERATED_BODY()
+	
+};
