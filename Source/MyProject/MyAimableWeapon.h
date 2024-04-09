9320c99222d6c9f1e8ebd5c478cbfa0b9fa6e528
Colibri Shin <colibrishin@gmail.com>
2024-04-09 19:18:16 +0900

    feat: add hitscan weapon

--- Source/MyProject/MyAimableWeapon.h
+++ Source/MyProject/MyAimableWeapon.h
@@ -3,8 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Utilities.hpp"
+
 #include "MyProject/MyWeapon.h"
 #include "MyAimableWeapon.generated.h"
+
+DECLARE_MULTICAST_DELEGATE(FOnFireReady)
 
 /**
  * 
@@ -13,5 +17,37 @@ UCLASS()
 class MYPROJECT_API AMyAimableWeapon : public AMyWeapon
 {
 	GENERATED_BODY()
+
+public:
+	AMyAimableWeapon();
+
+	void Fire();
+
+	DECL_BINDON(OnFireReady)
+
+private:
+	void ResetFire();
+
+	UPROPERTY(VisibleAnywhere)
+	float Range = 1000.0f;
+
+	UPROPERTY(VisibleAnywhere)
+	int32 Ammo = 100;
+
+	UPROPERTY(VisibleAnywhere)
+	int32 MaxAmmo = 100;
+
+	UPROPERTY(VisibleAnywhere)
+	int32 Magazine = 10;
+
+	UPROPERTY(VisibleAnywhere)
+	float FireRate = 0.1f;
+
+	UPROPERTY(VisibleAnywhere)
+	bool bCanFire;
+
+	FTimerHandle FireRateTimerHandle;
+
+	FOnFireReady OnFireReady;
 	
 };
