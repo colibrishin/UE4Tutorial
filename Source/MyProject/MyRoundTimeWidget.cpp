2065d9d09471e7a09d775413afe760ec32239739
Colibri Shin <colibrishin@gmail.com>
2024-04-11 16:26:32 +0900

    feat: add round time widget

---
+++ Source/MyProject/MyRoundTimeWidget.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyProject/MyRoundTimeWidget.h"
+
+#include "MyProjectGameModeBase.h"
+
+#include "Components/TextBlock.h"
+
+#include "GameFramework/GameStateBase.h"
+
+void UMyRoundTimeWidget::UpdateTime() const
+{
+	const float Time = AMyProjectGameModeBase::MatchRoundTime - GetWorld()->GetGameState()->GetServerWorldTimeSeconds();
+
+	const int32 Minutes = FMath::FloorToInt(Time / 60);
+	const int32 Seconds = FMath::FloorToInt(FMath::Fmod(Time, 60));
+	const FString TimeString = FString::Printf(TEXT("%02d:%02d"), Minutes, Seconds);
+
+	RoundTimeText->SetText(FText::FromString(TimeString));
+}
+
+void UMyRoundTimeWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+	UpdateTime();
+}
