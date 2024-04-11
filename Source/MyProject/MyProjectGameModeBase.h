9e623cd4d96fa570a481099f7c874deb93c316a4
Colibri Shin <colibrishin@gmail.com>
2024-04-11 16:27:15 +0900

    feat/todo: round management

--- Source/MyProject/MyProjectGameModeBase.h
+++ Source/MyProject/MyProjectGameModeBase.h
@@ -3,18 +3,31 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameModeBase.h"
+
+#include "GameFramework/GameMode.h"
 #include "MyProjectGameModeBase.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class MYPROJECT_API AMyProjectGameModeBase : public AGameModeBase
+class MYPROJECT_API AMyProjectGameModeBase : public AGameMode
 {
 	GENERATED_BODY()
 
 public:
+	static constexpr float MatchStartDelay = 5.f;
+	static constexpr float MatchRoundTime = 300.f;
+
 	AMyProjectGameModeBase();
-	
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	virtual void HandleMatchHasStarted() override;
+
+	virtual void HandleMatchHasEnded() override;
+
 };
