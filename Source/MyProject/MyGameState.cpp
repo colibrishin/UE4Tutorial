bd03ad8d8bbb2651d94b94f917242879889a3442
Colibri Shin <colibrishin@gmail.com>
2024-04-16 20:32:37 +0900

    feat: tracking alive players

--- Source/MyProject/MyGameState.cpp
+++ Source/MyProject/MyGameState.cpp
@@ -3,15 +3,21 @@
 
 #include "MyProject/MyGameState.h"
 
+#include "MyInGameHUD.h"
+#include "MyPlayerState.h"
 #include "MyProjectGameModeBase.h"
+#include "MyStatComponent.h"
 
 #include "Net/UnrealNetwork.h"
 
 AMyGameState::AMyGameState()
-	: bCanBuy(false),
-	  RoundProgress(EMyRoundProgress::Unknown)
+	: RoundProgress(EMyRoundProgress::Unknown),
+	  bCanBuy(false),
+	  AliveCT(0),
+	  AliveT(0)
 {
-	static ConstructorHelpers::FObjectFinder<USoundWave> RoundStartSoundFinder(TEXT("SoundWave'/Game/Models/sounds/moveout.moveout'"));
+	static ConstructorHelpers::FObjectFinder<USoundWave> RoundStartSoundFinder
+		(TEXT("SoundWave'/Game/Models/sounds/moveout.moveout'"));
 	RoundStartSound = RoundStartSoundFinder.Object;
 }
 
@@ -25,17 +31,74 @@ void AMyGameState::BuyTimeEnded()
 	}
 }
 
-void AMyGameState::OnRep_RoundProgress() const
+void AMyGameState::OnRep_RoundProgress()
 {
 	if (RoundProgress == EMyRoundProgress::Playing)
 	{
 		UGameplayStatics::PlaySound2D(this, RoundStartSound);
+		const auto& Players = GetWorld()->GetGameState()->PlayerArray;
+
+		for (const auto& Player : Players)
+		{
+			const auto& MyPlayerState = Cast<AMyPlayerState>(Player);
+
+			if (IsValid(MyPlayerState))
+			{
+				switch (MyPlayerState->GetTeam())
+				{
+				case EMyTeam::CT: 
+					++AliveCT;
+					break;
+				case EMyTeam::T:
+					++AliveT;
+					break;
+				case EMyTeam::Unknown:
+				default: break;
+				}
+
+				const auto& Handle = MyPlayerState->BindOnStateChanged(this, &AMyGameState::HandlePlayerStateChanged);
+				PlayerStateDelegateHandles.Add(Player->GetPlayerId(), Handle);
+			}
+		}
 	}
 }
 
 void AMyGameState::OnRep_CanBuy() const
 {
 	OnBuyChanged.Broadcast(bCanBuy);
+}
+
+void AMyGameState::HandlePlayerStateChanged(const EMyTeam Team, const EMyCharacterState State)
+{
+	switch (State)
+	{
+	case EMyCharacterState::Alive:
+		switch (Team)
+		{
+		case EMyTeam::CT: ++AliveCT;
+			break;
+		case EMyTeam::T: ++AliveT;
+			break;
+		case EMyTeam::Unknown: break;
+		}
+		break;
+	case EMyCharacterState::Planting: 
+		break;
+	case EMyCharacterState::Defusing: 
+		break;
+	case EMyCharacterState::Dead:
+		switch (Team)
+		{
+			case EMyTeam::CT: --AliveCT;
+			break;
+			case EMyTeam::T: --AliveT;
+			break;
+			case EMyTeam::Unknown: break;
+		}
+		break;
+	case EMyCharacterState::Unknown:
+	default: break;
+	}
 }
 
 void AMyGameState::BeginPlay()
@@ -55,6 +118,8 @@ void AMyGameState::BeginPlay()
 			false
 		);
 	}
+
+	//SetLifeSpan(AMyProjectGameModeBase::MatchRoundTime);
 }
 
 void AMyGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
@@ -62,4 +127,6 @@ void AMyGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLife
 	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
 	DOREPLIFETIME(AMyGameState, bCanBuy);
 	DOREPLIFETIME(AMyGameState, RoundProgress);
+	DOREPLIFETIME(AMyGameState, AliveCT);
+	DOREPLIFETIME(AMyGameState, AliveT);
 }
