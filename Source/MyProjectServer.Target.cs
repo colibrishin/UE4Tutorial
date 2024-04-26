4d1fca0d147aafb7e33a1854bd63a19262b533c8
Colibri Shin <colibrishin@gmail.com>
2024-04-26 13:45:12 +0900

    build: client and server targeting

---
+++ Source/MyProjectServer.Target.cs
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class MyProjectServerTarget : TargetRules
+{
+	public MyProjectServerTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		ExtraModuleNames.AddRange( new string[] { "MyProject" } );
+
+		bUseLoggingInShipping = true; 
+	}
+}
