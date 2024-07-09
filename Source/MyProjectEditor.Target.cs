34287f1ec194e953ec5b02998c0b97ab185b4768
Colibri Shin <colibrishin@gmail.com>
2024-07-09 23:59:08 +0900

    update to 5.4.2

--- Source/MyProjectEditor.Target.cs
+++ Source/MyProjectEditor.Target.cs
@@ -8,7 +8,8 @@ public class MyProjectEditorTarget : TargetRules
 	public MyProjectEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 		ExtraModuleNames.AddRange( new string[] { "MyProject" } );
 	}
 }
