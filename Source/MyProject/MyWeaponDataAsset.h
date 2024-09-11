72b5ba944dc76ea992eedbc9431a270abeedb0ca
Colibri Shin <colibrishin@gmail.com>
2024-09-12 01:41:38 +0900

    refactor: weapon data asset inherits from collectable

--- Source/MyProject/MyWeaponDataAsset.h
+++ Source/MyProject/MyWeaponDataAsset.h
@@ -3,7 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Engine/DataAsset.h"
+#include "MyCollectableDataAsset.h"
 #include "Private/Data.h"
 
 #include "MyWeaponDataAsset.generated.h"
@@ -13,34 +13,19 @@ class AMyWeapon;
  * 
  */
 UCLASS()
-class MYPROJECT_API UMyWeaponDataAsset : public UPrimaryDataAsset
+class MYPROJECT_API UMyWeaponDataAsset : public UMyCollectableDataAsset
 {
 	GENERATED_BODY()
 
 public:
-	void SetID(const int32 InID) { ID = InID; }
-	
-	int32 GetID() const { return ID; }
-	USkeletalMesh* GetSkeletalMesh() const { return WeaponSkeletalMesh; }
-	UStaticMesh* GetStaticMesh() const { return WeaponStaticMesh; }
 	USoundBase* GetFireSound() const { return WeaponFireSound; }
 	USoundBase* GetReloadSound() const { return WeaponReloadSound; }
 	const FMyWeaponStat& GetWeaponStat() const { return WeaponStat; }
 
-	bool IsSkeletal() const { return WeaponSkeletalMesh != nullptr; }
 	bool HasFireSound() const { return WeaponFireSound != nullptr; }
 	bool HasReloadSound() const { return WeaponReloadSound != nullptr; }
 
 private:
-	UPROPERTY(VisibleAnywhere)
-	int32 ID;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess))
-	USkeletalMesh* WeaponSkeletalMesh; 
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess))
-	UStaticMesh* WeaponStaticMesh;
-
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta=(AllowPrivateAccess))
 	USoundBase* WeaponFireSound;
 
